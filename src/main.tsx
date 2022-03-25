6622a51d1a3ff38b2e8db3938392cb030364468b
weilantian <xiaolanlub@outlook.com>
2022-03-25 15:18:25 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 /*
-React framework will be ejected to the site in this file.
+React framework will be injected to the site in this file.
 The reason for using the react framework in the particular tasks is because 
 it is a website that carries massive amount of web elements.
 React allows me to splint the code into small chunks, and it is easier to maintain.
